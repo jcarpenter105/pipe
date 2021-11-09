bb4a89fdb069e41fdea3a2b3bef0733e166f0d32
jcarpenter105 <78928260+jcarpenter105@users.noreply.github.com>
2021-11-08 21:16:26 -0500

    Added comments

--- main.cpp
+++ main.cpp
@@ -3,22 +3,22 @@
 #include <thread>
 #include <csignal>
 
-static bool escape = false;
+static bool escape = false; // flag for exiting the program
 
 class server {
-
 public:
 
-    std::string filepath;
+    std::string filepath; // holds the path the piped file will be stored
 
     explicit server(char* args[]) {
         if (args[1])
             filepath = args[1];
     }
-
+    
+    // start() opens a file, scans for user input, then prints the user input to both command prompt and piped file
     void start() const {
-        std::ofstream pipefile;
-        std::string textstream;
+        std::ofstream pipefile; // The piped file
+        std::string textstream; // One line of text from the original file
         pipefile.open("example.txt", std::ios_base::app);
         while(!escape) {
             std::getline(std::cin, textstream);
@@ -28,15 +28,15 @@ public:
         pipefile.close();
     }
 
+    // stop sets the flag to exit the while loop located in start
     static void stop(int signum){
-        std::cout<<"test"<<std::endl;
         escape = true;
     }
 };
 
 int main(int argc, char* argv[]) {
-    signal(SIGINT, server::stop);
-    server server(argv);
+    signal(SIGINT, server::stop); // declaring signal for interrupt (CTL+C)
+    server server(argv); // initializing the server class with the first command line argument
     std::thread t1(&server::start, server);
     t1.join();
     return 0;
